48d9e78d6ed0c8f01356e5927db70508e111ee35
rioto.nagano <kingdom950927@gmail.com>
2023-04-15 16:12:38 +0900

    UserとOrderデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = [
+  [1, "たけし", "aaa@gmail.com", "aaaaaa", "東京都北区1丁目◯番地", "080-1234-5678", Date.new(2023, 4, 30), "男"],
+  [2, "さおり", "bbb@gmail.com", "bbbbbb", "東京都北区1丁目◯番地", "080-1234-5678", Date.new(2023, 4, 30), "女"],
+  [3, "たろう", "ccc@gmail.com", "cccccc", "東京都北区1丁目◯番地", "080-1234-5678", Date.new(2023, 4, 30), "ゴリラ"]
+]
+
+users.each do |id, name, email, password_digest, address, phone_number, birthday, gender|
+  User.create(id: id, name: name, email: email, password_digest: password_digest, address: address, phone_number: phone_number, birthday: birthday, gender: gender)
+end
+
+orders = [
+  [1, 200, 0, "田中さとし", "東京都北区1丁目◯番地", Date.new(2023, 4, 30), Time.now],
+  [2, 230, 1, "パンナコッタ・フーゴ", "東京都北区2丁目◯番地", Date.new(2023, 3, 15), Time.now],
+  [3, 220, 2, "ピカチュウ", "東京都北区3丁目◯番地", Date.new(2013, 2, 19), Time.now]
+]
+
+orders.each do |user_id, total_price, payment, order_name, order_address, delivery_date, delivery_time|
+  Order.create(user_id: user_id, total_price: total_price, payment: payment, order_name: order_name, order_address: order_address, delivery_date: delivery_date, delivery_time: delivery_time)
+end
