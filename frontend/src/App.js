e321151491a32e220662924ba82c3ab69667c994
rioto.nagano <kingdom950927@gmail.com>
2023-04-15 19:39:59 +0900

    仮のヘッダーの作成

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,24 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // components
 import Top from './components/common/Top';
+import Header from './components/modules/Header';
 import ItemList from './components/item/ItemList';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Top />
-        </Route>
-        <Route exact path="/items">
-          <ItemList />
-        </Route>
-      </Switch>
-    </Router>
+    <div className='App'>
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Top />
+          </Route>
+          <Route exact path="/items">
+            <ItemList />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
