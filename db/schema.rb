1f2a37e91605bbfed1588e1bab91dfcd1eaffa7e
rioto.nagano <kingdom950927@gmail.com>
2023-04-15 14:55:22 +0900

    Categoryモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_15_054508) do
+ActiveRecord::Schema.define(version: 2023_04_15_055150) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
