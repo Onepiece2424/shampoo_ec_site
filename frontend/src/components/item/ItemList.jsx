1b935de8f4226b46c355b8fe89e5dbfbdbbd538b
rioto.nagano <kingdom950927@gmail.com>
2023-04-16 08:21:43 +0900

    商品名の表示

--- frontend/src/components/item/ItemList.jsx
+++ frontend/src/components/item/ItemList.jsx
@@ -1,20 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 // function
 import { fetchItems } from '../../apis/items'
 
 const ItemList = () => {
+  const [list, setList] = useState([])
 
   // 商品一覧データの取得
   useEffect(() => {
-    fetchItems()
-    .then((data) =>
-      console.log(data)
-    )
+    // fetchItems()
+    // .then((data) =>
+    //   setItems(data)
+    // )
+    const fetchData = async () => {
+      const data = await fetchItems();
+      setList(data);
+    };
+    fetchData();
   }, [])
+  console.log(list.items)
   return (
     <div>
-      Itemページです。
+      商品一覧ページです。
+      {list && list.items && list.items.map((item) => {
+        return <div key={item.id}>{item.name}</div>;
+      })}
     </div>
   )
 }
