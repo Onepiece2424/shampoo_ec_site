fc9e0164187efd430bc4c035bb0080ed3a0c41d1
rioto.nagano <kingdom950927@gmail.com>
2023-04-16 05:43:53 +0900

    fetchItemsDetail関数を非同期関数に変更

--- frontend/src/apis/itemsDetail.js
+++ frontend/src/apis/itemsDetail.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import { itemsDetail } from '../urls/index'
 
-export const fetchItemsDetail = (id) => {
-  return axios.get(itemsDetail(id))
-  .then(res => {
-    return res.data
-  })
-  .catch((e) => console.error(e))
+export const fetchItemsDetail = async (id) => {
+  try {
+    const res = await axios.get(itemsDetail(id));
+    return res.data;
+  } catch (e) {
+    return console.error(e);
+  }
 }
