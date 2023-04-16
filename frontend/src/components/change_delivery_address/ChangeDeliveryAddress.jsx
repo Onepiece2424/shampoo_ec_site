e8a1ddf9068346275fb08ae7162ec6b825b01295
rioto.nagano <kingdom950927@gmail.com>
2023-04-16 16:48:25 +0900

    お届け先変更とクレカ登録のためのcomponentの作成

---
+++ frontend/src/components/change_delivery_address/ChangeDeliveryAddress.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChangeDeliveryAddress = () => {
+  return (
+    <div>
+      お届け先変更入力フォームです。
+    </div>
+  )
+}
+
+export default ChangeDeliveryAddress
---
+++ frontend/src/components/credit/Credit.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Credit = () => {
+  return (
+    <div>
+      クレカ登録入力フォームです。
+    </div>
+  )
+}
+
+export default Credit
