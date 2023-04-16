f7309f64735ac23aeca6c3975daea5ee55ec0e7a
rioto.nagano <kingdom950927@gmail.com>
2023-04-16 16:28:48 +0900

    Cart modelとcontrollerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :orders, only: :index
       resources :users, only: :index
+      resources :carts, only: :index
       resources :items
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_15_060744) do
+ActiveRecord::Schema.define(version: 2023_04_16_072600) do
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -63,6 +70,7 @@ ActiveRecord::Schema.define(version: 2023_04_15_060744) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "carts", "users"
   add_foreign_key "order_details", "items"
   add_foreign_key "order_details", "orders"
   add_foreign_key "orders", "users"
