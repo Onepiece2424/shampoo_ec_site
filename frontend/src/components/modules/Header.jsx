0317a77ce80e36257ec77b25d5251f1b9bd04096
rioto.nagano <kingdom950927@gmail.com>
2023-04-23 07:18:27 +0900

    ヘッダーのレスポンシブ対応

--- frontend/src/components/modules/Header.jsx
+++ frontend/src/components/modules/Header.jsx
@@ -1,30 +1,12 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+const Header = () => {
 
-export default function ButtonAppBar() {
-  const classes = useStyles();
   const navigate = useNavigate();
 
   // ロゴクリック時に"/"へページ遷移
@@ -38,31 +20,30 @@ export default function ButtonAppBar() {
   }
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleHomeClick}>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleHomeClick}>
             SHOP LOGO
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-          </Typography>
-          <Button color="inherit" onClick={handleCartClick}>
-            <ShoppingCartOutlinedIcon />
-          </Button>
-          <Button color="inherit">
-            <FavoriteBorderOutlinedIcon />
-          </Button>
-          <Button color="inherit">
-            <PersonOutlineOutlinedIcon />
-          </Button>
-          <Button color="inherit">
-            1200 ポイント
-          </Button>
-          <Button color="inherit">
-            <MenuIcon />
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} />
+        <IconButton color="inherit" aria-label="cart" sx={{ mr: 2 }} onClick={handleCartClick}>
+          <ShoppingCartOutlinedIcon />
+        </IconButton>
+        <IconButton color="inherit" aria-label="favorite" sx={{ mr: 2 }}>
+          <FavoriteBorderOutlinedIcon />
+        </IconButton>
+        <IconButton
+          size="large"
+          edge="end"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   );
-}
+};
+
+export default Header
