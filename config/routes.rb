dc91d18a71d5b6fffecbfde105a68ac78721b954
rioto.nagano <kingdom950927@gmail.com>
2023-04-30 10:31:33 +0900

    rails g devise_token_auth:install User auth実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  # devise_for :users, skip: [:registrations]
   namespace :api do
     namespace :v1 do
       devise_for :users
+      # resources :registrations, only: [:create]
+      # devise_for :users, controllers: {
+      #   registrations: 'registrations'
+      # }
+      mount_devise_token_auth_for 'User', at: 'auth'
+      as :user do
+        # Define routes for User within this block.
+      end
       resources :orders, only: :index
       resources :users, only: :index
       post 'carts/add_item' => 'carts#add_item'
