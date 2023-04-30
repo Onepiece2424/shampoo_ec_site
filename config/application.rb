42507aa57d76a1cdb607f8e2f3ce26eaf384327c
rioto.nagano <kingdom950927@gmail.com>
2023-05-01 06:24:18 +0900

    CSRF対策

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,11 @@ module ShampooEcSite
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # ActionDispatch::Flashをインストール
+    config.middleware.use ActionDispatch::Flash
+
+    # CSRF対策
+    config.action_controller.allow_forgery_protection = false
   end
 end
