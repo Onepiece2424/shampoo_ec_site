b4a867e5d27852777e82bc4c34cb06074f3f95b1
rioto.nagano <kingdom950927@gmail.com>
2023-04-30 17:58:25 +0900

    CSRF対策

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,12 +1,8 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
-
-  def sign_up
-    binding.pry
-  end
-
+  protect_from_forgery
   private
 
   def sign_up_params
-    params.permit(:email, :password, :password_confirmation, :name)
+    params.permit(:email, :name, :password, :password_confirmation)
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,4 +310,7 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  # deviseの各コントローラーが、ApplicationControllerではなくActionController::Baseを継承するようにする
+  config.parent_controller = 'ActionController::Base'
 end
