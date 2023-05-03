4c777f544b74a378d7d4ecb0d274d4f04b302b17
rioto.nagano <kingdom950927@gmail.com>
2023-05-03 18:00:29 +0900

    マイグレーションファイルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_23_034048) do
+ActiveRecord::Schema.define(version: 2023_04_16_075428) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id", null: false
