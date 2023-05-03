54fb7f589d576c9e94d3d6c3b25da667cf055b66
rioto.nagano <kingdom950927@gmail.com>
2023-05-03 21:32:11 +0900

    application_controller ::API→::Baseに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   include DeviseTokenAuth::Concerns::SetUserByToken
 
