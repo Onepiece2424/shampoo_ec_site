e02f8f41f97ff665867fa6ffe6ed4b04fc0f2385
rioto.nagano <kingdom950927@gmail.com>
2023-05-05 21:22:22 +0900

    カートに入れるボタンとご注文手続きへ遷移するためのボタンの作成

--- frontend/src/components/item/ItemDetail.jsx
+++ frontend/src/components/item/ItemDetail.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { fetchItemsDetail } from '../../apis/itemsDetail'
 import Typography from '@mui/material/Typography';
 import logo from '../..//20230416_シャンプー画像.jpg'
+import { Button } from '@mui/material';
 
 const ItemDetail = () => {
 
   const [item, setItem] = useState()
   const params = useParams();
   const id = params.id
+  const navigate = useNavigate();
 
   // 商品詳細データの取得
   useEffect(() => {
@@ -17,6 +19,11 @@ const ItemDetail = () => {
       setItem(data.items)
     )
   }, [id])
+
+  // 商品をカートに入れる
+  const InsertItemToCart = () => {
+    console.log('カートへ入れました。')
+  }
 
   return (
     <div className="contents">
@@ -49,6 +56,9 @@ const ItemDetail = () => {
             </Typography>
             <div>{item && item.stock}</div>
           </div>
+          <br></br>
+          <Button variant="outlined">カートに入れる</Button>
+          <Button variant="outlined" style={{ margin: '3mm'}} onClick={() => navigate('/order')}>ご注文手続きへ</Button>
         </div>
       </div>
     </div>
