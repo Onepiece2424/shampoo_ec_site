93e20a7d46f29313a5e3bed0ef313352863df35c
rioto.nagano <kingdom950927@gmail.com>
2023-05-05 16:10:49 +0900

    アドレスとパスワードの入力フォーム、ボタンの作成

--- frontend/src/components/sign_in/SignIn.jsx
+++ frontend/src/components/sign_in/SignIn.jsx
@@ -1,11 +1,58 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form';
+import { Button, TextField } from '@mui/material';
+
+const renderTextField = ({
+  input,
+  label,
+  placeholder,
+  meta: { touched, invalid, error },
+  ...custom
+}) => (
+  <TextField
+    label={label}
+    placeholder={placeholder}
+    error={touched && invalid}
+    helperText={touched && error}
+    {...input}
+    {...custom}
+  />
+);
 
 const SignIn = () => {
   return (
-    <div>
-      ログインページです。
-    </div>
+    <>
+      <br></br>
+      <div>
+        ログインページです。
+      </div>
+      <br></br>
+      <form>
+        <br></br>
+        <Field
+          name="email"
+          component={renderTextField}
+          label="メールアドレス"
+          placeholder="メールアドレスを入力してください。"
+          style={{ width: 230 }}
+        />
+        <br></br>
+        <br></br>
+        <Field
+          name="password"
+          component={renderTextField}
+          label="パスワード"
+          placeholder="パスワードを入力してください。"
+          style={{ width: 230 }}
+        />
+        <br></br>
+        <br></br>
+        <Button variant="outlined">ログイン</Button>
+      </form>
+    </>
   )
 }
 
-export default SignIn
+export default reduxForm({
+  form: 'signInForm',
+})(SignIn);
