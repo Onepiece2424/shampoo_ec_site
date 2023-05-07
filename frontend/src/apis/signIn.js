88b0dacd4314e2a73ec5ea1c51b95cc80cd7549f
rioto.nagano <kingdom950927@gmail.com>
2023-05-07 21:31:51 +0900

    ログイン時にaccess-tokenなどのデータを更新し、そのデータをlocalStorageへ保存

--- frontend/src/apis/signIn.js
+++ frontend/src/apis/signIn.js
@@ -9,7 +9,18 @@ export const verifyUserData = async(params, dispatch) => {
     await axios.post(loginIndex, params)
     .then(data => {
       dispatch(dispatchUserData(data));
-      dispatch(pageTransitionFlag(true))
+      dispatch(pageTransitionFlag(true));
+
+      // 取得したトークン情報をlocalStorageなどに保存
+      const { headers } = data;
+      const token = headers['access-token'];
+      const client = headers['client'];
+      const uid = headers['uid'];
+
+      localStorage.setItem('access-token', token);
+      localStorage.setItem('client', client);
+      localStorage.setItem('uid', uid);
+
     }).catch(error => {
       console.log(error);
     });
