4c5bb68f20ec39e361501fd4413e9dec1f87a014
rioto.nagano <kingdom950927@gmail.com>
2023-05-11 05:16:37 +0900

    ログイン時の処理を変更

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -8,23 +8,11 @@ class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
   end
 
   def create
-    # パラメータからメールアドレスとパスワードを取得
-    email = params[:email]
-    password = params[:password]
-
-    # ユーザーを認証して、トークンを発行
-    resource = User.find_for_database_authentication(email: email)
-    return render json: { errors: ['メールアドレスまたはパスワードが間違っています'] }, status: :unauthorized unless resource
-
-    valid_password = resource.valid_password?(password)
-    return render json: { errors: ['メールアドレスまたはパスワードが間違っています'] }, status: :unauthorized unless valid_password
-
-    token = resource.create_new_auth_token
-    response.set_header('access-token', token['access-token'])
-    response.set_header('client', token['client'])
-    response.set_header('expiry', token['expiry'])
-    response.set_header('uid', token['uid'])
-
-    render json: { success: true, data: resource.as_json }
+    user = User.find_by(email: params[:email])
+    if user&.valid_password?(params[:password])
+      render json: user, status: :ok
+    else
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
+    end
   end
 end
--- frontend/src/apis/signIn.js
+++ frontend/src/apis/signIn.js
@@ -7,19 +7,19 @@ import { pageTransitionFlag } from '../reducks/reducers/common';
 export const verifyUserData = async(params, dispatch) => {
   // return (dispatch) => {
     await axios.post(loginIndex, params)
-    .then(data => {
-      dispatch(dispatchUserData(data));
+    .then(response => {
+      dispatch(dispatchUserData(response));
       dispatch(pageTransitionFlag(true));
 
       // 取得したトークン情報をlocalStorageなどに保存
-      const { headers } = data;
-      const token = headers['access-token'];
-      const client = headers['client'];
-      const uid = headers['uid'];
+      // const { data } = response;
+      // const token = data['access-token'];
+      // const client = data['client'];
+      // const uid = data['uid'];
 
-      localStorage.setItem('access-token', token);
-      localStorage.setItem('client', client);
-      localStorage.setItem('uid', uid);
+      // localStorage.setItem('access-token', token);
+      // localStorage.setItem('client', client);
+      // localStorage.setItem('uid', uid);
 
     }).catch(error => {
       console.log(error);
