f61ac6174f099a1c0f53b05dec5d63265e9413ee
rioto.nagano <kingdom950927@gmail.com>
2023-05-14 20:57:10 +0900

    devise-token-authを用いてユーザー作成機能の作成

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,7 +1,41 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+
+  # 新規登録①
+  # def create
+  #   super do |resource|
+  #     if resource.persisted?
+  #       create_token_info_for(resource)
+  #     end
+  #   end
+  # end
+
+  # private
+
+  # def sign_up_params
+  #   params.permit(:email, :password, :password_confirmation, :name)
+  # end
+
+  # def create_token_info_for(resource)
+  #   token = resource.create_new_auth_token
+  #   resource.save
+  #   response.set_header('access-token', token['access-token'])
+  #   response.set_header('client', token['client'])
+  # end
+
+  # 新規登録②
+  after_action :set_token_info, only: [:create]
+
   private
 
   def sign_up_params
     params.permit(:email, :password, :password_confirmation, :name)
+  end
+
+  def set_token_info
+    return unless @resource.persisted?
+
+    token = @resource.create_new_auth_token
+    response.set_header('access-token', token['access-token'])
+    response.set_header('client', token['client'])
   end
 end
