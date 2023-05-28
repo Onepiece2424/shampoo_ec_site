342d61acb65f4e07ab9f3cc93a5910526e2423b1
rioto.nagano <kingdom950927@gmail.com>
2023-05-28 15:08:05 +0900

    カートデータの作成

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -4,6 +4,12 @@ module Api
       before_action :authenticate_api_v1_user!
 
       def create
+        if current_api_v1_user.cart.present?
+
+        else
+          Cart.create({ user_id: current_api_v1_user.id })
+        end
+
         render json: { users: current_api_v1_user }, status: :ok
       end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable
   include DeviseTokenAuth::Concerns::User
 
-  has_many :carts, dependent: :destroy
+  has_one :cart, dependent: :destroy
   has_many :orders, dependent: :destroy
 end
