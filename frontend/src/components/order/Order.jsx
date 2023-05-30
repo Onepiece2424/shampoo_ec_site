7ac0a2a1e440550726ae63b0e069b674dfb3b425
rioto.nagano <kingdom950927@gmail.com>
2023-05-30 22:22:43 +0900

    注文手続きページにお届け日時などのフォームの作成

--- frontend/src/components/order/Order.jsx
+++ frontend/src/components/order/Order.jsx
@@ -1,14 +1,65 @@
 import React from 'react'
 import store from '../../reducks/store'
+import { Field, reduxForm } from 'redux-form';
+// components
+import { renderTextField } from '../modules/renderTextField';
 
-const Addressee = () => {
+const Addressee = (props) => {
 
-  console.log(store.getState().count)
+  const { handleSubmit } = props;
+
   return (
     <div>
-      注文手続きのページです。
+      <br></br>
+      <br></br>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div>お届け先</div>
+          <Field
+            name="address"
+            component={renderTextField}
+            label="お届け先"
+            placeholder="お届け先を入力してください。"
+            style={{ width: 280 }}
+          />
+        </div>
+        <br></br>
+        <br></br>
+        <div>
+          <div>お届け希望日時</div>
+          <Field
+            name="appointed_delivery_date"
+            component={renderTextField}
+            label="お届け予定日"
+            placeholder="お届け予定日を入力してください。"
+            style={{ width: 280 }}
+          />
+          <br></br>
+          <Field
+            name="appointed_delivery_time"
+            component={renderTextField}
+            label="お届け希望時間帯"
+            placeholder="お届け希望時間帯を入力してください。"
+            style={{ width: 280 }}
+          />
+        </div>
+        <br></br>
+        <br></br>
+        <div>
+          <div>支払い方法</div>
+          <Field
+            name="how_to_payment"
+            component={renderTextField}
+            label="支払い方法"
+            placeholder="支払い方法を入力してください。"
+            style={{ width: 280 }}
+          />
+        </div>
+      </form>
     </div>
   )
 }
 
-export default Addressee
+export default reduxForm({
+  form: 'orderForm',
+})(Addressee);
