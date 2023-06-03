a7e354568950998d09d1b1865f3830c322d15526
rioto.nagano <kingdom950927@gmail.com>
2023-06-03 16:18:52 +0900

    fix:不要な部分を削除

--- frontend/src/apis/createCart.js
+++ frontend/src/apis/createCart.js
@@ -1,17 +1,13 @@
 import axios from 'axios';
 import { createCarturl } from '../urls/index'
-// import { dispatchUserData } from '../reducks/reducers/user';
-// import { pageTransitionFlag } from '../reducks/reducers/common';
 
 // カート作成と商品追加
 export const createCart = async(ItemName, quantity) => {
-  // return (dispatch) => {
-    const params = { name: ItemName, quantity: quantity }
-    await axios.post(createCarturl, params)
-    .then(data => {
-      console.log(data)
-    }).catch(error => {
-      console.log(error);
-    });
-  // }
+  const params = { name: ItemName, quantity: quantity }
+  await axios.post(createCarturl, params)
+  .then(data => {
+    console.log(data)
+  }).catch(error => {
+    console.log(error);
+  });
 };
--- frontend/src/apis/fetchUserDara.js
+++ frontend/src/apis/fetchUserDara.js
@@ -1,17 +1,14 @@
 import axios from 'axios';
 import { userDataUrl } from '../urls/index'
 import { dispatchUserData } from '../reducks/reducers/user';
-// import { pageTransitionFlag } from '../reducks/reducers/common';
 
 // ログイン情報取得
 export const fetchUserData = async(userData, dispatch) => {
-  // return (dispatch) => {
-    await axios.post(userDataUrl, userData)
-    .then(data => {
-      console.log(data)
-      dispatch(dispatchUserData(data));
-    }).catch(error => {
-      console.log(error);
-    });
-  // }
+  await axios.post(userDataUrl, userData)
+  .then(data => {
+    console.log(data)
+    dispatch(dispatchUserData(data));
+  }).catch(error => {
+    console.log(error);
+  });
 };
