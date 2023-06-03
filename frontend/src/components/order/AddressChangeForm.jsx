78817d142ee5db3c9d7c20c8addda89e24721576
rioto.nagano <kingdom950927@gmail.com>
2023-06-04 08:09:52 +0900

    feat:styleをstyeld-compoentに書き換え、styleの管理を簡易化

--- frontend/src/components/order/AddressChangeForm.jsx
+++ frontend/src/components/order/AddressChangeForm.jsx
@@ -1,51 +1,58 @@
-import React from 'react'
+import React from 'react';
 import { Button } from '@mui/material';
 import { Field, reduxForm } from 'redux-form';
 import { renderTextField } from '../modules/renderTextField';
+import styled from 'styled-components';
+
+const ModalWrapper = styled.div`
+  background-color: #fff;
+  padding: 10px 75px;
+  border-radius: 10px;
+`;
+
+const FieldWrapper = styled.div`
+  margin: 10px 0;
+`;
+
+const CloseButton = styled(Button)`
+  margin: 0 10px;
+`;
 
 const AddressChangeForm = ({ Modal, close }) => {
-
-  // モーダルのスタイル
-  const modalStyle = {
-    backgroundColor: '#fff',
-    padding: '10px 75px',
-    borderRadius: '10px',
-  };
-
   return (
     <Modal>
-      <div style={modalStyle}>
+      <ModalWrapper>
         <h3>お届け先の変更</h3>
         <div>
-          <div style={{margin: '10px 0'}}>
+          <FieldWrapper>
             <Field name="receiver_name" component={renderTextField} label="受取人名" />
-          </div>
-          <div style={{margin: '10px 0'}}>
+          </FieldWrapper>
+          <FieldWrapper>
             <Field name="phone_number" component={renderTextField} label="電話番号" />
-          </div>
-          <div style={{margin: '10px 0'}}>
+          </FieldWrapper>
+          <FieldWrapper>
             <Field name="post_code" component={renderTextField} label="郵便番号" />
-          </div>
-          <div style={{margin: '10px 0'}}>
+          </FieldWrapper>
+          <FieldWrapper>
             <Field name="prefectures" component={renderTextField} label="都道府県" />
-          </div>
-          <div style={{margin: '10px 0'}}>
+          </FieldWrapper>
+          <FieldWrapper>
             <Field name="municipality" component={renderTextField} label="市町村" />
-          </div>
-          <div style={{margin: '10px 0'}}>
+          </FieldWrapper>
+          <FieldWrapper>
             <Field name="street_number" component={renderTextField} label="丁目・番地・号" />
-          </div>
-          <div style={{margin: '10px 0'}}>
+          </FieldWrapper>
+          <FieldWrapper>
             <Field name="building_name" component={renderTextField} label="建物名" />
-          </div>
+          </FieldWrapper>
         </div>
         <div>
-          <Button onClick={close} style={{ margin: '0 10px' }}>閉じる</Button>
-          <Button onClick={close} style={{ margin: '0 10px' }}>変更する</Button>
+          <CloseButton onClick={close}>閉じる</CloseButton>
+          <CloseButton onClick={close}>変更する</CloseButton>
         </div>
-      </div>
+      </ModalWrapper>
     </Modal>
-  )
-}
+  );
+};
 
-export default AddressChangeForm
+export default AddressChangeForm;
--- frontend/src/components/order/Order.jsx
+++ frontend/src/components/order/Order.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { useModal } from 'react-hooks-use-modal';
 import { Button, Card } from '@mui/material';
+import styled from 'styled-components';
 
 // components
 import DatePickerField from './DatePickerField';
@@ -9,45 +10,56 @@ import TimePickerField from './TimePickerField';
 import renderRadioGroup from './renderRadioGroup';
 import AddressChangeForm from './AddressChangeForm';
 
-const Addressee = (props) => {
+const Wrapper = styled.div`
+  margin: 10px 0;
+`;
 
+const StyledCard = styled(Card)`
+  margin: 10px 40px;
+`;
+
+const StyledButton = styled(Button)`
+  margin: 20px 0;
+`;
+
+const Addressee = (props) => {
   const { handleSubmit } = props;
 
   const [Modal, open, close, isOpen] = useModal('root', {
-    preventScroll: true
+    preventScroll: true,
   });
 
   return (
     <div>
-      <br></br>
-      <br></br>
       <form onSubmit={handleSubmit}>
         <div>
           <h3>お届け先</h3>
-          <div style={{ margin: '10px 0' }}>
-            <Card variant="outlined" style={{ margin: '10px 40px' }} >
+          <Wrapper>
+            <StyledCard variant="outlined">
               <div>氏名：テスト太郎</div>
               <div>郵便番号：111-1111</div>
               <div>電話番号：090-1111-1111</div>
-            </Card>
-          </div>
-          <div style={{ margin: '20px 0' }}>
-            <Button variant="outlined" onClick={open}>お届け先を変更する</Button>
+            </StyledCard>
+          </Wrapper>
+          <div>
+            <StyledButton variant="outlined" onClick={open}>
+              お届け先を変更する
+            </StyledButton>
           </div>
           <AddressChangeForm Modal={Modal} isOpen={isOpen} close={close} />
         </div>
-        <br></br>
+        <br />
         <div>
           <h3>お届け希望日時</h3>
-          <div style={{margin: '10px 0'}}>
+          <Wrapper>
             <Field
               name="appointed_delivery_date"
               component={DatePickerField}
               label="お届け予定日"
               placeholder="お届け予定日を入力してください。"
             />
-          </div>
-          <br></br>
+          </Wrapper>
+          <br />
           <div>
             <Field
               name="appointed_delivery_time"
@@ -57,8 +69,8 @@ const Addressee = (props) => {
             />
           </div>
         </div>
-        <br></br>
-        <br></br>
+        <br />
+        <br />
         <div>
           <h3>支払い方法</h3>
           <div>
@@ -72,8 +84,8 @@ const Addressee = (props) => {
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default reduxForm({
   form: 'orderForm',
