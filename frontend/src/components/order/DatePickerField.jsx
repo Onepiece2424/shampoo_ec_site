74284ef3c99fd20f8ec05bf272a4141e9e5ccaa3
rioto.nagano <kingdom950927@gmail.com>
2023-06-04 17:37:29 +0900

    feat:選択した日付のフォーマットを変更

--- frontend/src/components/order/DatePickerField.jsx
+++ frontend/src/components/order/DatePickerField.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
-// import DatePicker from '@mui/lab/DatePicker';
 import { DatePicker } from '@mui/x-date-pickers';
 import TextField from '@mui/material/TextField';
+import moment from 'moment';
 
 const DatePickerField = ({
   input: { value, onChange },
   meta: { touched, error },
   ...custom
 }) => {
+
+  // 選択した日付を「YYYY-MM-DD」の形に変換
+  const handleDateChange = (selectedDate) => {
+    if (selectedDate) {
+      const formattedDate = moment(selectedDate.$d).format('YYYY-MM-DD');
+      onChange(formattedDate);
+    } else {
+      onChange(null);
+    }
+  };
+
+  const parseInputValue = (value) => {
+    if (value) {
+      return moment(value).toDate();
+    }
+    return null;
+  };
+
   return (
     <div>
       <DatePicker
         {...custom}
-        value={value || null}
-        onChange={onChange}
+        value={parseInputValue(value)}
+        onChange={handleDateChange}
         textField={(params) =>
           <TextField {...params} error={touched && error} />
         }
