9ccdd49a70022254b609aad11b044806dff0c93a
rioto.nagano <kingdom950927@gmail.com>
2023-06-07 05:48:06 +0900

    fix:各値に振り分ける数値の変更

--- frontend/src/components/order/TimePickerField.jsx
+++ frontend/src/components/order/TimePickerField.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 
 const availableTimeRanges = [
-  { label: '午前中', start: '00:00', end: '12:00' },
-  { label: '12:00 - 14:00', start: '12:00', end: '14:00', value: 0 },
-  { label: '14:00 - 16:00', start: '14:00', end: '16:00', value: 1 },
-  { label: '16:00 - 18:00', start: '16:00', end: '18:00', value: 2 },
-  { label: '18:00 - 20:00', start: '18:00', end: '20:00', value: 3 }
+  { label: '午前中', start: '00:00', end: '12:00', value: 0 },
+  { label: '12:00 - 14:00', start: '12:00', end: '14:00', value: 1 },
+  { label: '14:00 - 16:00', start: '14:00', end: '16:00', value: 2 },
+  { label: '16:00 - 18:00', start: '16:00', end: '18:00', value: 3 },
+  { label: '18:00 - 20:00', start: '18:00', end: '20:00', value: 4 }
 ];
 
 const TimePickerField = ({ input, label, meta: { touched, error } }) => (
