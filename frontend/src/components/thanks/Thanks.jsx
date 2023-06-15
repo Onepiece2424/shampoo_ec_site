1810d05dbd67071a87e35d214948f7ab23cde8ac
rioto.nagano <kingdom950927@gmail.com>
2023-06-16 05:45:44 +0900

    feat:トップへ戻るボタンや買い物を続けるボタンの作成

--- frontend/src/components/thanks/Thanks.jsx
+++ frontend/src/components/thanks/Thanks.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Button } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 
 const Thanks = () => {
+
+  const navigate = useNavigate()
+
   return (
-    <div className='thanks-text'>
-      注文完了しました。ご注文ありがとうございました。
+    <div>
+      <div className='thanks-text'>
+        注文完了しました。ご注文ありがとうございました。
+      </div>
+      <Button variant="outlined" style={{ margin: '3mm'}} onClick={() => navigate('/')}>トップへ戻る</Button>
+      <Button variant="outlined" style={{ margin: '3mm'}} onClick={() => navigate('/items')}>買い物を続ける</Button>
     </div>
   )
 }
