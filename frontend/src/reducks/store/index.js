fc90951c67217cc594f82e592798514498d2f82b
rioto.nagano <kingdom950927@gmail.com>
2023-06-17 13:17:03 +0900

    storeの中身を修正

--- frontend/src/reducks/store/index.js
+++ frontend/src/reducks/store/index.js
@@ -1,23 +1,26 @@
 import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
 
-// reducer
+// reducers
 import { reducer as reduxFormReducer } from 'redux-form';
 import { userReducer } from "../reducers/user";
 import { flagReducer } from "../reducers/common";
 import { cartReducer } from "../reducers/cart";
 import { cartItemReducer } from "../reducers/cart_items";
 
-export const rootReducer = combineReducers({
+const rootReducer = combineReducers({
   form: reduxFormReducer,
   user: userReducer,
   pageFlag: flagReducer,
   cart: cartReducer,
   cart_items: cartItemReducer,
-})
+});
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+const store = createStore(
+  rootReducer,
+  composeEnhancer(applyMiddleware(thunk))
+);
 
 export default store;
