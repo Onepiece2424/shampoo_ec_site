2550fbc10c77233fddef0a92a7c0700915b11377
rioto.nagano <kingdom950927@gmail.com>
2023-06-17 18:48:25 +0900

    feat:ハンバーガメニュークリック時にサイドバーの表示

--- frontend/src/components/modules/Header.jsx
+++ frontend/src/components/modules/Header.jsx
@@ -1,49 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
 const Header = () => {
-
   const navigate = useNavigate();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  // ロゴクリック時に"/"へページ遷移
   const handleHomeClick = () => {
     navigate('/');
   };
 
-  // カートページへ
   const handleCartClick = () => {
     navigate('/carts');
-  }
+  };
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleHomeClick}>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleHomeClick}
+          >
             SHOP LOGO
           </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} />
-        <IconButton color="inherit" aria-label="cart" sx={{ mr: 2 }} onClick={handleCartClick}>
-          <ShoppingCartOutlinedIcon />
-        </IconButton>
-        <IconButton color="inherit" aria-label="favorite" sx={{ mr: 2 }}>
-          <FavoriteBorderOutlinedIcon />
-        </IconButton>
-        <IconButton
-          size="large"
-          edge="end"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} />
+          <IconButton
+            color="inherit"
+            aria-label="cart"
+            sx={{ mr: 2 }}
+            onClick={handleCartClick}
+          >
+            <ShoppingCartOutlinedIcon />
+          </IconButton>
+          <IconButton color="inherit" aria-label="favorite" sx={{ mr: 2 }}>
+            <FavoriteBorderOutlinedIcon />
+          </IconButton>
+          <IconButton
+            size="large"
+            edge="end"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={toggleSidebar}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="right" open={isSidebarOpen} onClose={toggleSidebar}>
+        <List>
+          <ListItem button onClick={handleHomeClick}>
+            <ListItemIcon>
+              <MenuIcon />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
+          <ListItem button onClick={handleCartClick}>
+            <ListItemIcon>
+              <ShoppingCartOutlinedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Cart" />
+          </ListItem>
+          {/* Add more sidebar items as needed */}
+        </List>
+      </Drawer>
+    </>
   );
 };
 
-export default Header
+export default Header;
