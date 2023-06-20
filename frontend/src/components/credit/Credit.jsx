9df19aafe863e49529d5f62295e7e062b1a67893
rioto.nagano <kingdom950927@gmail.com>
2023-06-21 06:21:17 +0900

    feat:credit componentをモーダルとして仮作成

--- frontend/src/components/credit/Credit.jsx
+++ frontend/src/components/credit/Credit.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useModal } from 'react-hooks-use-modal';
+import styled from 'styled-components';
 
-const Credit = () => {
+const ModalWrapper = styled.div`
+  background-color: #fff;
+  padding: 10px 75px;
+  border-radius: 10px;
+`;
+
+const Credit = ({ Modal, close }) => {
+
   return (
-    <div>
-      クレカ登録入力フォームです。
-    </div>
+    <Modal>
+      <ModalWrapper>
+        <div className='credit-form'>
+          クレカ登録入力フォームです。
+        </div>
+      </ModalWrapper>
+    </Modal>
   )
 }
 
