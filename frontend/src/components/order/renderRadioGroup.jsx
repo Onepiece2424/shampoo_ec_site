5cda9c73d253b7c48ef405bbaeb36845de56b17a
rioto.nagano <kingdom950927@gmail.com>
2023-06-24 08:57:23 +0900

    feat:デフォルトのチェックを代引きにする

--- frontend/src/components/order/renderRadioGroup.jsx
+++ frontend/src/components/order/renderRadioGroup.jsx
@@ -5,19 +5,22 @@ import Credit from '../credit/Credit';
 const RenderRadioGroup = ({ input, meta: { touched, error } }) => {
 
   const [open, setOpen] = useState(false)
+  const [defaultval, IsDefaultVal] = useState(1)
 
   const creditOpen = () => {
     setOpen(true)
+    IsDefaultVal(0)
   }
 
   const creditClose = () => {
     setOpen(false)
+    IsDefaultVal(1)
   }
 
   return (
     <Box display="flex" justifyContent="center">
       <div>
-        <RadioGroup {...input} defaultValue={1}>
+        <RadioGroup {...input} value={defaultval}>
           <FormControlLabel value={1} control={<Radio />} label="代引き" onClick={creditClose} />
           <FormControlLabel value={0} control={<Radio />} label="クレジット" onClick={creditOpen} />
           {open && <Credit />}
