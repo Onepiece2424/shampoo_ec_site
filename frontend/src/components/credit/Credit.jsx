f8d92c0e2f31d64ed09e7abcb689c8af6e8e7937
rioto.nagano <kingdom950927@gmail.com>
2023-06-24 07:32:09 +0900

    feat:クレカフォームの作成

--- frontend/src/components/credit/Credit.jsx
+++ frontend/src/components/credit/Credit.jsx
@@ -1,12 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Cards from 'react-credit-cards-2';
+import { Field } from 'redux-form';
+import 'react-credit-cards-2/dist/es/styles-compiled.css';
 
-const Credit = () => {
+const getFields = () => [
+  { type: 'number', name: 'number', placeholder: 'Card Number' },
+  { type: 'name', name: 'name', placeholder: 'Name' },
+  { type: 'number', name: 'expiry', placeholder: 'Card Expiry' },
+  { type: 'number', name: 'cvc', placeholder: 'Card CVC' },
+];
+
+const PaymentForm = ({ handleSubmit }) => {
+  const [focus, setFocus] = useState('');
+  const [formData, setFormData] = useState({
+    number: '',
+    expiry: '',
+    cvc: '',
+    name: '',
+  });
+  const fields = getFields();
+
+  const handleInputChange = (evt) => {
+    const { name, value } = evt.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleInputFocus = (evt) => {
+    setFocus(evt.target.name);
+  };
 
   return (
-    <div className='credit-form'>
-      クレカ登録入力フォームです。
+    <div>
+      <Cards focused={focus} {...formData} />
+      <form onSubmit={handleSubmit}>
+        {fields.map((field) => (
+          <div style={ {margin : '10px'} }>
+            <Field
+              key={field.name}
+              component='input'
+              type={field.type}
+              name={field.name}
+              placeholder={field.placeholder}
+              onChange={handleInputChange}
+              onFocus={handleInputFocus}
+            />
+          </div>
+        ))}
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Credit
+export default PaymentForm;
