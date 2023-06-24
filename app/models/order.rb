f811a719dc734a915d4fbfd845c7ec76e0805d07
rioto.nagano <kingdom950927@gmail.com>
2023-06-24 12:24:11 +0900

    feat:total_priceが1000円以上のOrderのみ抽出するためのscopeを作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,6 @@ class Order < ApplicationRecord
 
   enum payment: { クレジット: 0, 代引き: 1 }
   enum delivery_time: { 午前中: 0, "12:00 - 14:00": 1, "14:00 - 16:00": 2, "16:00 - 18:00": 3, "18:00 - 20:00": 4 }
+
+  scope :higher_total_price, -> { where('total_price > ?', 1000) } # total_priceが1000円以上の注文のみ抽出
 end
