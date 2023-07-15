de8aa301f5869812fd247909668c9dc001fb4d33
rioto.nagano <kingdom950927@gmail.com>
2023-07-16 06:12:02 +0900

    feat:時間を日本時間に設定

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,8 @@ module ShampooEcSite
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
+
+    config.time_zone = 'Tokyo'
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
