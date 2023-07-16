c192862783c4dba14b018120d1c0d22880f11e36
rioto.nagano <kingdom950927@gmail.com>
2023-07-16 09:09:35 +0900

    fix:不要なCSRF対策のためのコードを削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
   include DeviseTokenAuth::Concerns::SetUserByToken
-
-  skip_before_action :verify_authenticity_token
-  helper_method :current_user, :user_signed_in?
 end
