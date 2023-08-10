667be4b0b4f94f6deb335c4a71cdaf71378b859b
rioto.nagano <kingdom950927@gmail.com>
2023-08-11 07:38:07 +0900

    refactor:不要な部分を削除

--- frontend/src/apis/signIn.js
+++ frontend/src/apis/signIn.js
@@ -5,26 +5,20 @@ import { pageTransitionFlag } from '../reducks/reducers/common';
 
 // ログイン認証処理
 export const verifyUserData = async(params, dispatch) => {
-  // return (dispatch) => {
-    await axios.post(loginIndex, params)
-    .then(response => {
-      dispatch(dispatchUserData(response.data));
-      dispatch(pageTransitionFlag(true));
+  await axios.post(loginIndex, params)
+  .then(response => {
+    dispatch(dispatchUserData(response.data));
+    dispatch(pageTransitionFlag(true));
 
-      // 取得したresponseからdataプロパティのuserプロパティの中にあるaccess_tokenなどを分割代入で抽出
-      // eslint-disable-next-line
-      // const { data: { user, user: { access_token, client, uid } } } = response;
+    // 取得したトークン情報をlocalStorageに保存
+    localStorage.setItem('access-token', response.data.data.access_token);
+    localStorage.setItem('client', response.data.data.client);
+    localStorage.setItem('uid', response.data.data.uid);
 
-      // 取得したトークン情報をlocalStorageなどに保存
-      localStorage.setItem('access-token', response.data.data.access_token);
-      localStorage.setItem('client', response.data.data.client);
-      localStorage.setItem('uid', response.data.data.uid);
+    alert('ログイン成功しました。')
 
-      alert('ログイン成功しました。')
-
-    }).catch(error => {
-      console.log(error);
-      alert('ログイン失敗しました。')
-    });
-  // }
+  }).catch(error => {
+    console.log(error);
+    alert('ログイン失敗しました。')
+  });
 };
