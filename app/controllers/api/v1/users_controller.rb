86191364d550398a942677facfe14d885ffcf550
rioto.nagano <kingdom950927@gmail.com>
2023-08-13 14:18:43 +0900

    feat:current_api_v1_userを用いたデータ取得

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,10 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      before_action :authenticate_api_v1_user!
       def index
-        @user = User.where(uid: params[:headers][:uid])
+        # @user = User.where(uid: params[:headers][:uid])
+        @user = current_api_v1_user
         if @user.present?
           render json: { data: @user }, status: :ok
         else
--- frontend/src/apis/fetchUserDara.js
+++ frontend/src/apis/fetchUserDara.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
-import { userDetailDataUrl } from '../urls/index'
+import { userDetailDataUrl, homeUrl } from '../urls/index'
 import { fetchUserDataCreator } from '../reducks/reducers/user';
 import { fetchCartItemData } from './fetchCartItem';
 
 // ログイン情報取得
 export const fetchUserData = async(headers, dispatch) => {
-  await axios.get(userDetailDataUrl, {params: {headers: headers}})
+  await axios.get(userDetailDataUrl, { headers: headers })
+  // await axios.get(userDetailDataUrl)
+  // await axios.get(homeUrl)
   .then(response => {
     dispatch(fetchUserDataCreator(response.data));
 
